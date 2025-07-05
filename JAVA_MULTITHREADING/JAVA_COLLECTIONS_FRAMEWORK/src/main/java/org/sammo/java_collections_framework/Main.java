2be2f306094312900f19cfeeb9a4a36179ba16c2
SoyaChunkz <sameermaroof26@gmail.com>
2025-07-05 23:34:33 +0530

    💣

--- JAVA_MULTITHREADING/JAVA_COLLECTIONS_FRAMEWORK/src/main/java/org/sammo/java_collections_framework/Main.java
+++
@@ -1,7 +0,0 @@
-package org.sammo.java_collections_framework;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- JAVA_MULTITHREADING/OOPS/bank/Accounts.java
+++
@@ -1,8 +0,0 @@
-package OOPS.bank;
-
-public class Accounts{
-    public String name = "abcd";
-    private int id = 63;
-    protected int age = 20;
-}
-
